9133448a538fb771c0fa77dc48a256744ad51935
Nicolas <nicolas.g.ferreira@edu.senai.br>
2026-08-18 11:32:05 -0300

    Salvando

---
+++ 01-funcoes/05-funcao-condicional.js
@@ -0,0 +1,9 @@
+function verificaNota(media){
+    // if(media >= 7){
+    //     return "Aprovado"
+    // }
+    // return "Reprovado"
+ return media >= 7 ? "Aprovado" : "Reprovado"
+}
+
+console.log(verificarNota(8));
---
+++ 01-funcoes/06-functions-expressions.js
@@ -0,0 +1,10 @@
+function somarTradicional(a, b){
+    return a + b
+}
+
+const somar = function (a, b){
+    return a + b
+}
+
+console.log(somarTradicional(10,5))
+console.log(somaAnonima(10,5))
