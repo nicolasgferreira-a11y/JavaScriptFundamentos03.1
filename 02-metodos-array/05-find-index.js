b30567acc8be058ff3420bc82ccbd1b5dba8c366
Nicolas <nicolas.g.ferreira@edu.senai.br>
2026-08-18 14:12:43 -0300

    gg

---
+++ 02-metodos-array/05-find-index.js
@@ -0,0 +1,9 @@
+let nomes = ["João", "Julia", "Larissa", "Livia"]
+
+const indice = nomes.findIndex((n) => n === "Larissa")
+
+console.log(indice)
+
+const indice = nomes.findIndex((n) => n === "Matheus")
+
+console.log(indice)
