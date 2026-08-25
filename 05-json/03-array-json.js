883c78cf95a0f88e49f106ae67d11691536f81fc
Nicolas <nicolas.g.ferreira@edu.senai.br>
2026-08-25 08:50:41 -0300

    SalvandoMudacas

--- 05-json/03-array-json.js
+++ 05-json/03-array-json.js
@@ -2,3 +2,7 @@ const alunos = [
   {nome: "Ana", nota: 9},
   {nome: "Alice", nota: 7},
 ];
+
+alunos.forEach((a)=> console.log(`O aluno(a) ${a.nome} a nota: ${a.nota}`))
+
+const json = JSON.stringify(alunos);
