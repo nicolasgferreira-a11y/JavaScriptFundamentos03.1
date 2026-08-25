aeab8ae38211de9008e54c31d5c5b5478b4b49f0
Nicolas <nicolas.g.ferreira@edu.senai.br>
2026-08-25 09:22:12 -0300

    SalvandoMudacas

---
+++ 06-introdução-poo/01-classes.js
@@ -0,0 +1,5 @@
+class pessoa{
+
+}
+const pessoa = new pessoa()
+console.log(pessoa)
---
+++ 06-introdução-poo/02-construtor.js
@@ -0,0 +1,8 @@
+class pessoa{
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+const pessoa = new pessoa("Giovani", 16)
