1cd243971332913506f9d96f05344fcda0936605
Nicolas <nicolas.g.ferreira@edu.senai.br>
2026-08-25 11:21:33 -0300

    SalvandoMudacas

---
+++ 07-poo/07-desafio.js
@@ -0,0 +1,21 @@
+class Aluno{
+    constructor(nome, nota1, nota2){
+        this.nome = nome;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    calcularMedia(){
+        return (this.nota1 + this.nota2) / 2;
+    }
+
+    verificarSituacao(){
+        return this.calcularMedia() >= 7 ? "Aprovado" : "Reprovado";
+    }
+
+    exibirDados(){
+        console.log(`
+        ${this.nome} | Média: ${this.calcularMedia() | this.verificarSituacao()}
+        `)
+    }
+}
