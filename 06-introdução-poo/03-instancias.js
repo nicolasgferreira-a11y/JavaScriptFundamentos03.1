9068ffdd2b40e91674a0735a48d0e32807280d1a
Nicolas <nicolas.g.ferreira@edu.senai.br>
2026-08-25 10:40:54 -0300

    SalvandoMudacas

---
+++ 06-introdução-poo/03-instancias.js
@@ -0,0 +1,8 @@
+const aluno = new Aluno("Carlos", "Desenvolvimento de Sistemas");
+console.log(aluno);
+        this.disciplina = disciplina;
+    }
+}
+
+const professor = new Professor("Marcos", 5000, "Programação");
+console.log(professor);
