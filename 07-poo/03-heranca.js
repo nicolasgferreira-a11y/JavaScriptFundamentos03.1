0ec634240453346cfe8044b6dbbdf280272ba3b8
Nicolas <nicolas.g.ferreira@edu.senai.br>
2026-08-25 10:41:39 -0300

    SalvandoMudacas

---
+++ 07-poo/03-heranca.js
@@ -0,0 +1,6 @@
+const aluno = new Aluno("Carlos", "Desenvolvimento de Sistemas");
+console.log(aluno);
+        this.disciplina = disciplina;
+
+const professor = new Professor("Marcos", 5000, "Programação");
+console.log(professor);
